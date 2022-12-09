7d9b1f1ae246d7a130aafa4c88f3e738051a6ac9
amangoyal018 <goyalamanmay18@gmail.com>
2022-12-09 21:38:44 +0530

    codeforces*day5

---
+++ CodeForces/A._Beautiful_Year.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    string year1,year2;
+    cin>>year1;
+    int year11=stoi(year1);
+
+    for(int i=year11;i<=9000;i++){
+
+    }
+
+}            
