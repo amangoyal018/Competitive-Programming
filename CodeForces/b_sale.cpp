f0254be9c8fd2e697d3362c97b66e727bdfc179e
amangoyal018 <goyalamanmay18@gmail.com>
2023-03-17 00:50:46 +0530

    fsfsdf

--- CodeForces/b_sale.cpp
+++ CodeForces/b_sale.cpp
@@ -9,6 +9,34 @@ int main()
 #endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int n,m;
+    cin>>n>>m;
+
+    int count=0;
+    int sum=0;
+
+    vector <int> v;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a)
+    }
+    
+
+    for(int i=0;i<n;i++){
+        if(count==m){
+            break;
+        }
+        int a;
+        cin>>a;
+        if(a<0){
+            sum+=a;
+            count++;
+
+        }
+    }
+    cout<<abs(sum);
+
 
     
 }
