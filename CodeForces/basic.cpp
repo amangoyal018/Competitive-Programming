508acb267cd92e7cb40bfa9292f14c28cf94a106
amangoyal018 <goyalamanmay18@gmail.com>
2024-02-22 22:53:54 +0530

    custom_hash

--- CodeForces/basic.cpp
+++ CodeForces/basic.cpp
@@ -57,6 +57,20 @@ bool isPrime(int n)
 }
 bool ispow2(int x){return (x ? !(x & (x - 1)) : 0);} 
 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
 
 void solve(){
 
